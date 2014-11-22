8ea6a222aacfcee6986b4d4f0ccdc1e5b7c9628c
Eric Katz <emkatz1989@gmail.com>
2014-11-21 21:11:01 -0800

    blah

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -10,7 +10,7 @@ class Stream < ActiveRecord::Base
   	# Validate the attached image is image/jpg, image/png, etc
   	validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-  	# validates_presence_of :name
+  	 validates_presence_of :name
   	# validates_presence_of :description
   	# validates_presence_of :image
 
